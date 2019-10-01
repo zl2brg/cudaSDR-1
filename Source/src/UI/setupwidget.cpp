d11601e8ee7e7f631e926fb2fc67f35c35ffd419
Simon Eatough <simon.eatough@pertronic.co.nz>
2019-10-02 11:43:32 +1300

    Added configuration ui files

---
+++ Source/src/UI/setupwidget.cpp
@@ -0,0 +1,7 @@
+#include "setupwidget.h"
+#include "ui_setupwidget.h"
+
+SetupWigdet::SetupWigdet(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ Source/src/UI/setupwidget.h
@@ -0,0 +1,84 @@
+#ifndef SETUPWIGDET_H
+#define SETUPWIGDET_H
+
+#include <QWidget>
+#include <QWidget>
+#include "Util/cusdr_buttons.h"
+#include "cusdr_settings.h"
+#include "cusdr_fonts.h"
+
+
+namespace Ui {
+    class SetupWidget;
+}
+
+class SetupWidget : public QWidget
+{
+Q_OBJECT
+
+public:
+    SetupWidget(QWidget *parent = 0);
+    ~SetupWidget();
+
+private:
+    Ui::SetupWidget *ui;
+    Settings	*set;
+    QSDR::_ServerMode			m_serverMode;
+    QSDR::_HWInterfaceMode		m_hwInterface;
+    QSDR::_DataEngineState		m_dataEngineState;
+
+    QList<TReceiver>	m_rxDataList;
+    void        getSettings();
+    CFonts		*fonts;
+    TFonts		m_fonts;
+
+    int     m_rx;
+    int		m_minimumWidgetWidth;
+    int		m_minimumGroupBoxWidth;
+    int		m_btnSpacing;
+    int		m_fontHeight;
+    int		m_maxFontWidth;
+    int		m_currentReceiver;
+    bool	m_mouseOver;
+
+    void	setupConnections();
+
+
+private slots:
+    void	systemStateChanged(
+            QObject *sender,
+            QSDR::_Error err,
+            QSDR::_HWInterfaceMode hwmode,
+            QSDR::_ServerMode mode,
+            QSDR::_DataEngineState state);
+
+    void	setCurrentReceiver(QObject *sender, int rx);
+    void    nfModeChanged(int value);
+    void    nbModeChanged(int value);
+    void    nr2GainChanged(int value);
+    void    agcProcChanged(int value);
+    void    snbChanged(bool value);
+    void    anfChanged(bool value);
+    void    nr2aeChanged(bool value);
+    void    omsChanged(bool value);
+    void    mmseChanged(bool value);
+    void    preAgcChanged(bool value);
+    void    postAgcChanged(bool nalue);
+    void    npeModeChanged(int value);
+
+
+
+
+
+
+public slots:
+    QSize	sizeHint() const;
+    QSize	minimumSizeHint() const;
+
+
+
+};
+
+
+
+#endif // SETUPWIGDET_H
